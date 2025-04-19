2f0a69107b99abbd78ae562f46160d475c9772d5
Ruslan A. <ruslan.absalyamov@gmail.com>
2025-04-19 23:31:28 +0200

    Refactor FileExplorerCommand (Test)

--- src/test/kotlin/org/examples/testbot/commands/FileExplorerCommand.kt
+++ src/test/kotlin/org/examples/testbot/commands/FileExplorerCommand.kt
@@ -18,81 +18,94 @@ class FileExplorerCommand : BaseCommand() {
         }
 
         currentDir.set(nextPath)
-        val currentFile = File(nextPath)
+        val nextFile = File(nextPath)
 
         log.debug("message: {}, currentPath: {}, nextPath: {}", message, currentPath, nextPath)
 
-        if (currentFile.isDirectory) {
-            val files = currentFile.listFiles() ?: emptyArray()
-            files.sortBy { it.name }
-            val allFiles = files.map { SubCommand.of(it.name, if (it.isDirectory) "[${it.name}]" else it.name) }
-                .toMutableList()
-            val progressInfo = progressInfo.get()
-            val progressStatus = progressInfo?.let {
-
-                val st = if (it.status.isNotBlank()) "```\nStatus: ${it.status}\n```" else ""
-
-                """Size: ${FileUtils.byteCountToDisplaySize(it.size)}
-                   |Progress: ${it.percent}%
-                   |$st""".trimMargin()
-            } ?: ""
-
-            if (progressInfo != null) {
-                allFiles.add(0, SubCommand.of(SystemCommands.NOTHING, title = "Calculating...${progressInfo.percent}%"))
-            } else {
-                allFiles.add(
-                    0, SubCommand.of<CalculateDirSizeCommand>(
-                        state = StateRef.of(nextPath),
-                        title = "Calculate dir size"
-                    )
-                )
-            }
-
-            if (nextPath != "/") {
-                allFiles.add(0, SubCommand.of(UP_DIR))
-            }
-
-            val subCommands = mutableListOf<List<SubCommand>>()
-            val rowSize = 5
-
-            for (i in 0..allFiles.size / rowSize) {
-                val index = i * rowSize
-                subCommands.add(allFiles.subList(index, Math.min(index + rowSize, allFiles.size)))
-            }
-            context.page(
-                """
-                Files of directory: *$nextPath*
-                Count: ${allFiles.size - 1}
-                $progressStatus
-                """.trimIndent()
-            )
-                .messageType(MessageType.Inline)
-                .contentType(ContentType.Markdown)
-                .subCommands(subCommands)
-                .enableBack()
-                .update()
+        if (nextFile.isDirectory) {
+            handleDirectory(nextFile, progressInfo)
         } else {
-            val lastModified = Date(currentFile.lastModified())
-
-            context.page(
-                """
-                        Information about file: *$nextPath*
-                        Size: ${currentFile.length()} bytes
-                        Last modified: $lastModified
-                    """.trimIndent()
-            )
-                .contentType(ContentType.Markdown)
-                .messageType(MessageType.Inline)
-                .subCommands(
-                    SubCommand.of(UP_DIR),
-                    SubCommand.of<FileDownloadCommand>(
-                        title = "Download",
-                        state = StateRef.of(nextPath)
-                    )
-                )
-                .enableBack()
-                .update()
+            handleFile(nextFile)
         }
+    }
+
+    private fun handleDirectory(
+        directory: File,
+        progressInfo: State<CalculateDirSizeProgressInfo>
+    ) {
+        val nextPath: String = directory.absolutePath
+        val files = directory.listFiles() ?: emptyArray()
+        files.sortBy { it.name }
+        val allFiles = files.map { SubCommand.of(it.name, if (it.isDirectory) "[${it.name}]" else it.name) }
+            .toMutableList()
+        val progressInfo = progressInfo.get()
+        val progressStatus = progressInfo?.let {
+
+            val st = if (it.status.isNotBlank()) "```\nStatus: ${it.status}\n```" else ""
+
+            """Size: ${FileUtils.byteCountToDisplaySize(it.size)}
+                       |Progress: ${it.percent}%
+                       |$st""".trimMargin()
+        } ?: ""
+
+        if (progressInfo != null) {
+            allFiles.add(0, SubCommand.of(SystemCommands.NOTHING, title = "Calculating...${progressInfo.percent}%"))
+        } else {
+            allFiles.add(
+                0, SubCommand.of<CalculateDirSizeCommand>(
+                    state = StateRef.of(nextPath),
+                    title = "Calculate dir size"
+                )
+            )
+        }
+
+        if (nextPath != "/") {
+            allFiles.add(0, SubCommand.of(UP_DIR))
+        }
+
+        val subCommands = mutableListOf<List<SubCommand>>()
+        val rowSize = 5
+
+        for (i in 0..allFiles.size / rowSize) {
+            val index = i * rowSize
+            subCommands.add(allFiles.subList(index, Math.min(index + rowSize, allFiles.size)))
+        }
+        context.page(
+            """
+                    Files of directory: *$nextPath*
+                    Count: ${allFiles.size - 1}
+                    $progressStatus
+                    """.trimIndent()
+        )
+            .messageType(MessageType.Inline)
+            .contentType(ContentType.Markdown)
+            .subCommands(subCommands)
+            .enableBack()
+            .update()
+    }
+
+    private fun handleFile(file: File) {
+        val lastModified = Date(file.lastModified())
+        val nextPath: String = file.absolutePath
+
+        context.page(
+            """
+            Information about file: *$nextPath*
+            Size: ${file.length()} bytes
+            Last modified: $lastModified
+            """.trimIndent()
+        )
+            .contentType(ContentType.Markdown)
+            .messageType(MessageType.Inline)
+            .subCommands(
+                SubCommand.of(UP_DIR),
+                SubCommand.of<FileDownloadCommand>(
+                    title = "Download",
+                    state = StateRef.of(nextPath)
+                )
+            )
+            .enableBack()
+            .update()
     }
 
     companion object {
